60add0a984f009a8cc805d103aa6c2b25bcc5331
Ruckard Smith <ruckard.smith@gmail.com>
2022-05-22 13:04:39 +0200

    New reread settings.

--- reread-spanish.sh
+++ reread-spanish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./nerd-dictation begin --reread --reread --reread-rate 70 --reread-voice 'spanish-mbrola-1' --reread-volume=1 --reread-pitch=20 --vosk-model-dir='/home/playg/vosk-models/vosk-model-small-es-0.22' --full-sentence --punctuate-from-previous-timeout 1 --idle-time 0.5 --continuous --timeout 0.5 --output=STDOUT > /tmp/prueba_salida_fichero.txt
+./nerd-dictation begin --reread --reread --reread-rate 78 --reread-voice 'spanish-mbrola-1' --reread-volume=1 --reread-pitch=20 --vosk-model-dir='/home/playg/vosk-models/vosk-model-small-es-0.22' --full-sentence --punctuate-from-previous-timeout 1 --idle-time 0.5 --continuous --timeout 0.5 --output=STDOUT > /tmp/prueba_salida_fichero.txt
