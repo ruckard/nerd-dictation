3a017e93dc63f3d02147ecbce2c2334c7dfbb40e
Ruckard Smith <ruckard.smith@gmail.com>
2022-05-23 00:27:07 +0200

    New better settings for reading aloud English.

--- reread-english.sh
+++ reread-english.sh
@@ -11,4 +11,4 @@
 #  5  en-us           --/M      us-mbrola-3        mb/mb-us3            (en 8)
 #  5  en-gb-x-gbcwmd  --/M      English_(West_Midlands) gmw/en-GB-x-gbcwmd   (en-gb 9)(en 9)
 
-./nerd-dictation begin --reread --reread-rate 76 --reread-voice 'us-mbrola-2' --reread-volume=1 --reread-pitch=40 --vosk-model-dir='/home/playg/vosk-models/vosk-model-small-en-us-0.15' --full-sentence --punctuate-from-previous-timeout 1 --idle-time 0.5 --continuous --timeout 0.5 --output=STDOUT > /tmp/prueba_salida_fichero.txt
+./nerd-dictation begin --reread --reread-rate 73 --reread-voice 'us-mbrola-2' --reread-volume=1 --reread-pitch=60 --vosk-model-dir='/home/playg/vosk-models/vosk-model-small-en-us-0.15' --full-sentence --punctuate-from-previous-timeout 1 --idle-time 0.5 --continuous --timeout 0.5 --output=STDOUT > /tmp/prueba_salida_fichero.txt
